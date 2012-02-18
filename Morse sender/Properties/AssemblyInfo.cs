bb436214dc0387c60f8ed0ce64da3a593d56e547
Lorenzo <Lorenzo@Lorenzo-PC.Home>
2012-02-18 21:18:22 +0100

    Dunno

--- Morse sender/Properties/AssemblyInfo.cs
+++ Morse sender/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Morse_sender")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © TheFerrango  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
