382b6b981390117848061b796af1a555f0475979
TheFerrango <llorenzo@outlook.com>
2016-02-04 20:37:24 +0100

    added Windows 10 UWP app

---
+++ UWP/Morse Sender/Morse Sender/MainPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Il modello di elemento per la pagina vuota è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
+
+namespace Morse_Sender
+{
+    /// <summary>
+    /// Pagina vuota che può essere utilizzata autonomamente oppure esplorata all'interno di un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+		AudioPlayer audioPlayer;
+		string tmpMorse;
+		bool isPlaying = false;
+		bool loaded;
+		bool toMorse;
+
+		public MainPage()
+        {
+            this.InitializeComponent();
+			audioPlayer = new AudioPlayer();
+			loaded = false;
+        }
+
+		private void txtToMorsefy_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			lblMorsed.Text = toMorse ? MorseConverters.ConvertToMorse(txtToMorsefy.Text) : MorseConverters.ConvertToText(txtToMorsefy.Text);
+		}
+
+		private async void Page_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (!loaded)
+			{
+				//if (!App.HasBeenStarted)
+				//{
+				//	MessageBox.Show(Langs.AppResources.MsgBoxStart);
+				//	App.HasBeenStarted = true;
+				//}
+				toMorse = true;
+				radioT2m.IsChecked = true;
+				txtToMorsefy_TextChanged(null, null);
+
+				StorageFile wavHolder = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Sound/dot.wav"));
+				await audioPlayer.LoadFileAsync(wavHolder);
+				
+				wavHolder = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Sound/dash.wav"));			
+				await audioPlayer.LoadFileAsync(wavHolder);
+
+				wavHolder = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Sound/smpause.wav"));
+				await audioPlayer.LoadFileAsync(wavHolder);
+
+				loaded = true;
+			}
+		}
+
+		private void radioT2m_Checked(object sender, RoutedEventArgs e)
+		{
+			toMorse = radioT2m.IsChecked == true ? true : false;
+			txtToMorsefy_TextChanged(null, null);
+		}
+
+		private void AppBarButtonShare_Tapped(object sender, TappedRoutedEventArgs e)
+		{
+			DataTransferManager dtm = DataTransferManager.GetForCurrentView();
+			dtm.DataRequested += Dtm_DataRequested;
+			DataTransferManager.ShowShareUI();
+		}
+
+		private void Dtm_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+		{
+			var rqst = args.Request;
+			rqst.Data.Properties.Title = "A Morse message";
+			rqst.Data.SetText(lblMorsed.Text);
+		}
+
+		private async void AppBarButtonPlay_Tapped(object sender, TappedRoutedEventArgs e)
+		{
+			if (radioT2m.IsChecked == true)
+			{
+				if (!isPlaying)
+				{
+					isPlaying = true;
+					tmpMorse = lblMorsed.Text;					
+					foreach (char ch in tmpMorse)
+						await Riproduci(ch);
+					isPlaying = false;
+				}
+			}
+		}
+
+		private async Task Riproduci(char ch)
+		{
+			switch (ch)
+			{
+				case '.': await audioPlayer.Play("dot.wav", 0.5); break;
+				case '-': await audioPlayer.Play("dash.wav", 0.5); break;
+				case ' ': await audioPlayer.Play("smpause.wav", 0.5); break;
+				default: break;
+			}
+		}
+	}
+}
---
+++ UWP/Morse Sender/Morse Sender/MorseConverters.cs
@@ -0,0 +1,134 @@
+﻿namespace Morse_Sender
+{
+	public static class MorseConverters
+  {
+    private static string Char2Morse(char c)
+    {
+      string morseChar = "";
+      switch (c)
+      {
+        case 'a': morseChar = ".-"; break;
+        case 'b': morseChar = "-..."; break;
+        case 'c': morseChar = "-.-."; break;
+        case 'd': morseChar = "-.."; break;
+        case 'e': morseChar = "."; break;
+        case 'f': morseChar = "..-."; break;
+        case 'g': morseChar = "--."; break;
+        case 'h': morseChar = "...."; break;
+        case 'i': morseChar = ".."; break;
+        case 'j': morseChar = ".---"; break;
+        case 'k': morseChar = "-.-"; break;
+        case 'l': morseChar = ".-.."; break;
+        case 'm': morseChar = "--"; break;
+        case 'n': morseChar = "-."; break;
+        case 'o': morseChar = "---"; break;
+        case 'p': morseChar = ".--."; break;
+        case 'q': morseChar = "--.-"; break;
+        case 'r': morseChar = ".-."; break;
+        case 's': morseChar = "..."; break;
+        case 't': morseChar = "-"; break;
+        case 'u': morseChar = "..-"; break;
+        case 'v': morseChar = "...-"; break;
+        case 'w': morseChar = ".--"; break;
+        case 'x': morseChar = "-..-"; break;
+        case 'y': morseChar = "-.--"; break;
+        case 'z': morseChar = "--.."; break;
+        case '0': morseChar = "-----"; break;
+        case '1': morseChar = ".----"; break;
+        case '2': morseChar = "..---"; break;
+        case '3': morseChar = "...--"; break;
+        case '4': morseChar = "....-"; break;
+        case '5': morseChar = "....."; break;
+        case '6': morseChar = "-...."; break;
+        case '7': morseChar = "--..."; break;
+        case '8': morseChar = "---.."; break;
+        case '9': morseChar = "----."; break;
+        case '.': morseChar = ".-.-"; break;
+        case ',': morseChar = "--..--"; break;
+        case '?': morseChar = "..--.."; break;
+        case ' ': morseChar = " / "; break;
+        default: morseChar = ""; break;
+      }
+
+      return morseChar + " ";
+
+    }
+
+    private static string Morse2Char(string letter)
+    {
+      string morseChar = "";
+      switch (letter)
+      {
+        case ".-": morseChar = "a"; break;
+        case "-...": morseChar = "b"; break;
+        case "-.-.": morseChar = "c"; break;
+        case "-..": morseChar = "d"; break;
+        case ".": morseChar = "e"; break;
+        case "..-.": morseChar = "f"; break;
+        case "--.": morseChar = "g"; break;
+        case "....": morseChar = "h"; break;
+        case "..": morseChar = "i"; break;
+        case ".---": morseChar = "j"; break;
+        case "-.-": morseChar = "k"; break;
+        case ".-..": morseChar = "l"; break;
+        case "--": morseChar = "m"; break;
+        case "-.": morseChar = "n"; break;
+        case "---": morseChar = "o"; break;
+        case ".--.": morseChar = "p"; break;
+        case "--.-": morseChar = "q"; break;
+        case ".-.": morseChar = "r"; break;
+        case "...": morseChar = "s"; break;
+        case "-": morseChar = "t"; break;
+        case "..-": morseChar = "u"; break;
+        case "...-": morseChar = "v"; break;
+        case ".--": morseChar = "w"; break;
+        case "-..-": morseChar = "x"; break;
+        case "-.--": morseChar = "y"; break;
+        case "--..": morseChar = "z"; break;
+        case "-----": morseChar = "0"; break;
+        case ".----": morseChar = "1"; break;
+        case "..---": morseChar = "2"; break;
+        case "...--": morseChar = "3"; break;
+        case "....-": morseChar = "4"; break;
+        case ".....": morseChar = "5"; break;
+        case "-....": morseChar = "6"; break;
+        case "--...": morseChar = "7"; break;
+        case "---..": morseChar = "8"; break;
+        case "----.": morseChar = "9"; break;
+        case ".-.-": morseChar = "."; break;
+        case "--..--": morseChar = ","; break;
+        case "..--..": morseChar = "?"; break;
+      }
+      return morseChar;
+    }
+
+    public static string ConvertToText(string toConvert)
+    {
+      string finale = "";
+      string[] words = toConvert.Split('/');
+      for (int i = 0; i < words.Length; i++)
+        finale += MorseToWord(words[i]);
+      return finale;
+    }
+
+    private static string MorseToWord(string p)
+    {
+      string finale = "";
+      string[] lettere = p.Split(' ');
+      foreach (string s in lettere)
+        finale += Morse2Char(s);
+      return finale + " ";
+    }
+
+    public static string ConvertToMorse(string toConvert)
+    {
+      toConvert = toConvert.ToLower();
+      string encoded = "";
+      foreach (char cara in toConvert)
+      {
+        encoded += Char2Morse(cara);
+      }
+      return encoded;
+    }
+  }
+}
